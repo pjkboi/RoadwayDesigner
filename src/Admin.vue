8f23b21a999eaad184b7bf4a4cb743a65369cfe7
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2019-06-21 12:09:21 -0400

    formatting admin page

--- src/Admin.vue
+++ src/Admin.vue
@@ -1,38 +1,39 @@
 <template>
-<div id="app">
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="#694393">
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-              </v-toolbar>
-              <v-card-text>
-                <v-form >
-                  <v-text-field
-                    v-model="email"
-                    label="E-mail"
-                    required
-                  ></v-text-field>
-                  <v-text-field v-model="password" label="Password" id="password" type="password" required></v-text-field>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="#694393" style="color:white;" @click="onSubmit">Login</v-btn>
-                    <v-btn color="#694393" style="color:white;" @click="onReset">Clear</v-btn>
-                  </v-card-actions>
-                </v-form>
-              </v-card-text>
-              
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
-</div>
+  <div id="app">
+    <v-app id="inspire">
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+              <v-card class="elevation-12">
+                <v-toolbar dark color="#694393">
+                  <v-toolbar-title>Login form</v-toolbar-title>
+                  <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field v-model="email" label="E-mail" required></v-text-field>
+                    <v-text-field
+                      v-model="password"
+                      label="Password"
+                      id="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="#694393" style="color:white;" @click="onSubmit">Login</v-btn>
+                      <v-btn color="#694393" style="color:white;" @click="onReset">Clear</v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
 </template>
 <script>
 export default {
@@ -49,10 +50,7 @@ export default {
   },
   methods: {
     onSubmit(evt) {
-      if (
-        this.email == this.admin.name &&
-        this.password == this.admin.pw
-      ) {
+      if (this.email == this.admin.name && this.password == this.admin.pw) {
         this.$emit("Admin", this.show);
       } else {
         alert("Not the admin");
